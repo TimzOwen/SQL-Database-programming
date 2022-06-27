bd9a53dfafa2b1c05a7ef4a91b47821673265551
Timz Owen <timzowen@Timz.local>
2022-06-27 14:42:32 +0300

    query update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SQL-Database-programming
+## SQL-Database-programming
+
 Understanding core concepts of SQL programming and using databases in programming 
+
+In SQL programming, keywords are highlighted in uppercase to distinguish them from other words used.
+
+semi colons are used to indicate end of SQL statement or query;
--- beginner.sql
+++ beginner.sql
@@ -1,1 +1,12 @@
-select * from table_name;  /* show data in tables*/
+SELECT * FROM people;  /* show data in tables when you query a people table*/
+
+SELECT name FROM people /*displays names of in a table called people*/
+
+SELECT 'DataCamp <3 SQL'  /*selected as db name and displayed*/
+AS result;
+
+SELECT name, birthdate FROM people /*select multiple columns from same table*/
+
+SELECT * FROM people /*select all coumns from table people*/
+
+SELEC * FROM people LIMIT 10; /* select all columns with just 10 columns */
